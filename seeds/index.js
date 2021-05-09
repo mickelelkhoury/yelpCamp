bf67f8f7ffd08e398f65b5a124fd7540ab117560
Mickel <70326656+mickelelkhoury@users.noreply.github.com>
2021-05-09 11:35:01 +0300

    added ejs-mate engine and re-seeded DB

--- seeds/index.js
+++ seeds/index.js
@@ -25,9 +25,14 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20 + 10);
 		const camp = new Campground({
 			title: `${sample(descriptors)} ${sample(places)}`,
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			image: 'https://source.unsplash.com/collection/483251',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae iste aperiam excepturi repellendus minus, optio tempore voluptatum voluptate quidem mollitia ducimus atque illum aliquam quas! Eaque labore delectus omnis tempore aperiam in doloremque explicabo suscipit, ea vero iure fugiat porro corrupti voluptates illum aut voluptate cumque perferendis rem. Nostrum, blanditiis.',
+			price,
 		});
 		await camp.save();
 	}
