99d309d85142da8e784d2da6c00fd4cefcbe2710
Mickel <mickelelkhoury-work@outlook.com>
2021-06-02 12:23:44 +0300

    returnTo with login

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -10,13 +10,16 @@ router.get('/register', (req, res) => {
 
 router.post(
 	'/register',
-	catchAsync(async (req, res) => {
+	catchAsync(async (req, res, next) => {
 		try {
 			const { email, username, password } = req.body;
 			const user = new User({ email, username });
 			const registeredUser = await User.register(user, password);
-			req.flash('success', 'Welcome to YelpCamp!');
-			res.redirect('/campgrounds');
+			req.login(registeredUser, (errr) => {
+				if (err) return next(err);
+				req.flash('success', 'Welcome to YelpCamp!');
+				res.redirect('/campgrounds');
+			});
 		} catch (error) {
 			req.flash('error', error.message);
 			res.redirect('register');
@@ -36,7 +39,9 @@ router.post(
 	}),
 	(req, res) => {
 		req.flash('success', 'Welcome Back!');
-		res.redirect('/campgrounds');
+		const redirectUrl = req.session.returnTo || '/campgrounds';
+		delete req.session.returnTo;
+		res.redirect(redirectUrl);
 	}
 );
 
