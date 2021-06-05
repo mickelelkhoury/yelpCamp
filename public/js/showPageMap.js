441a31483fcf5e580fd7a342b1786d8faf40cace
Mickel <mickelelkhoury-work@outlook.com>
2021-06-05 14:09:17 +0300

    map style

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,9 +1,16 @@
 mapboxgl.accessToken = mapToken;
 let map = new mapboxgl.Map({
 	container: 'map',
-	style: 'mapbox://styles/mapbox/streets-v11',
+	style: 'mapbox://styles/mapbox/light-v10',
 	center: campground.geometry.coordinates,
 	zoom: 10,
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker({ color: '#317E07' })
+	.setLngLat(campground.geometry.coordinates)
+	.setPopup(
+		new mapboxgl.Popup({ offset: 25 }).setHTML(
+			`<h5>${campground.title}</h5><p>${campground.location}</p>`
+		)
+	)
+	.addTo(map);
