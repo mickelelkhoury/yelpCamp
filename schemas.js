a6f577b89bbd1a5653d60a802ab0b34490c8be0b
Mickel <mickelelkhoury-work@outlook.com>
2021-06-07 10:43:43 +0300

    Sanitizing HTML

--- schemas.js
+++ schemas.js
@@ -25,11 +25,10 @@ const Joi = BaseJoi.extend(extension)
 
 module.exports.campgroundSchema = Joi.object({
 	campground: Joi.object({
-		title: Joi.string().required(),
+		title: Joi.string().required().escapeHTML(),
 		price: Joi.number().required().min(0),
-		// images: Joi.string().required(),
-		location: Joi.string().required(),
-		description: Joi.string().required(),
+		location: Joi.string().required().escapeHTML(),
+		description: Joi.string().required().escapeHTML(),
 	}).required(),
 	deleteImages: Joi.array(),
 });
@@ -37,6 +36,6 @@ module.exports.campgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
 	review: Joi.object({
 		rating: Joi.number().required().min(1).max(5),
-		body: Joi.string().required().escapeHTML(),
+		body: Joi.string().required().escapeHTML().escapeHTML(),
 	}).required(),
 });
