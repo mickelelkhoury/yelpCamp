14aaff99e00ba82d2e4d879a942778633583dd54
Mickel <70326656+mickelelkhoury@users.noreply.github.com>
2021-06-07 12:59:27 +0300

    port change

--- app.js
+++ app.js
@@ -171,6 +171,8 @@ app.use((err, req, res, next) => {
 	res.status(statusCode).render('error/error', { err });
 });
 
-app.listen(3000, () => {
-	console.log('SERVING ON PORT 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`SERVING ON PORT ${PORT}`);
 });
